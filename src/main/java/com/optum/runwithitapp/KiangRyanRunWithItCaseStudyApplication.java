36d0216d1f1b2b3012e8ee0ff5398e485e21035d
Ryan <ryanlkiang@gmail.com>
2022-03-30 12:59:39 -0500

    re-initilized project to include new dependencies

---
+++ src/main/java/com/optum/runwithitapp/KiangRyanRunWithItCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.optum.runwithitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiangRyanRunWithItCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiangRyanRunWithItCaseStudyApplication.class, args);
+	}
+
+}
