163d9cc5f8821e08eee4b322163da358f01a7229
Ryan <ryanlkiang@gmail.com>
2022-04-11 15:35:53 -0500

    edited update exercise to postmapping and works on DOM

--- src/main/java/com/optum/runwithitapp/Controllers/ExercisesController.java
+++ src/main/java/com/optum/runwithitapp/Controllers/ExercisesController.java
@@ -5,17 +5,15 @@ import com.optum.runwithitapp.Services.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ExercisesController {
 
     private ExerciseService exerciseService;
 
-    public ExercisesController(){
+    public ExercisesController() {
 
     }
 
@@ -25,33 +23,33 @@ public class ExercisesController {
     }
 
     @GetMapping("/exercise")
-    public String getAllExercises(Model model){
+    public String getAllExercises(Model model) {
         model.addAttribute("listExercises", exerciseService.getAllExercises());
         return "exercise";
     }
 
     @GetMapping("/showExercises")
-    public String showExercisesForm(Model model){
+    public String showExercisesForm(Model model) {
         Exercises exercises = new Exercises();
         model.addAttribute("exercises", exercises);
         return "exercise";
     }
 
     @PostMapping("/createExercises")
-    public String saveNewExercises(@ModelAttribute("exercises") Exercises exercises){
+    public String saveNewExercises(@ModelAttribute("exercises") Exercises exercises) {
         exerciseService.saveExercises(exercises);
         return "redirect:/exercise";
     }
 
-    @GetMapping("/updatedExercises/{id}")
-    public String updateExercises(@PathVariable(value = "id") long exerciseId, Model model){
-        Exercises exercises = exerciseService.getExercisesById(exerciseId);
-        model.addAttribute("exercises", exercises);
-        return "exercise";
+    @PostMapping("/updatedExercises/{id}")
+    public String updateExercises(@PathVariable("id") long id, Exercises exercises,
+                                  Model model){
+        exerciseService.saveExercises(exercises);
+        return "redirect:/exercise";
     }
 
     @GetMapping("/deleteExercises/{id}")
-    public String deleteExercises(@PathVariable(value="id")long exerciseId){
+    public String deleteExercises(@PathVariable(value = "id") long exerciseId) {
         this.exerciseService.deleteExercisesById(exerciseId);
         return "redirect:/exercise";
     }
