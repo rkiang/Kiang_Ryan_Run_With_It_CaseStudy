cf43bd64701547da1c5777580786be6f685f0783
Ryan <ryanlkiang@gmail.com>
2022-04-14 09:20:57 -0500

    uncommented visibility for second confirm password field

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -1,15 +1,15 @@
 function pswdVisible() {
     var x = document.getElementById("inputPassword3");
-//            var y = document.getElementById("password2");
+            var y = document.getElementById("inputPassword4");
 
     if (x.type  === "password") {
         x.type = "text";
         } else {
             x.type = "password";
         }
-//            if (y.type  === "password") {
-//                y.type = "text";
-//              } else {
-//                y.type = "password";
-//              }
+            if (y.type  === "password") {
+                y.type = "text";
+              } else {
+                y.type = "password";
+              }
  }
