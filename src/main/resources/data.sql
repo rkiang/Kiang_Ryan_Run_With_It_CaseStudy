07a0007ec8bcd57ae7bcd547e76e4003ea5b9e08
Ryan <ryanlkiang@gmail.com>
2022-04-25 15:35:17 -0500

    SQL data.sql will load on start, and create DB if does not exist.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 -- To add Admin role privileges
 INSERT INTO user (id, email, first_name, last_name, password)
-VALUES (1, "admin@email.com", "first", "last", "admin");
+--Inserted password is "admin"
+VALUES (1, "admin@admin.com", "first", "last", "{bcrypt}$2a$10$MpMM/qjPrAJTw2K3jM4PgOMAzKkbTk/FmOy4pT5V.BSJB//ryCTjK");
 INSERT INTO role (id, name) VALUES (1, "ROLE_ADMIN");
-INSERT INTO users_roles (user_id, role_id) VALUES (1, 1)
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
